c9f5ed6074eded5d15c3b174672c5089256c3b45
Brian the Lion <rossabri@hotmail.com>
2010-06-07 13:28:05 -0700

    Just the basics.

---
+++ __init__.py
@@ -0,0 +1,4 @@
+'''
+At some point, I'd like this __init__ file to be smart enough to do all the
+relevant edits to routing.py, middleware.py, etc, etc.
+'''
---
+++ setup.sh
@@ -0,0 +1,4 @@
+mkdir rest && cd rest && git clone git://github.com/sciyoshi/pyfacebook.git;
+
+
+mkdir graph && cd graph && git clone git://github.com/facebook/python-sdk.git;
