3aef6824c6206fb681eab5f04165b5595f546146
Brian the Lion <rossabri@hotmail.com>
2010-06-07 13:50:06 -0700

    check_config

--- __init__.py
+++ __init__.py
@@ -2,3 +2,18 @@
 At some point, I'd like this __init__ file to be smart enough to do all the
 relevant edits to routing.py, middleware.py, etc, etc.
 '''
+
+REQUIRED = ['facebook.apikey','facebook.secret','facebook.callback','facebook.url']
+
+def check_config() :
+	
+	from pylons import config
+	class ConfigError(Exception) : pass	
+
+	if not all( [config.has_key(r) for r in REQUIRED] ) : raise ConfigError()
+	if not all( [config[r] for r in REQUIRED] ) : raise ConfigError()
+	
+check_config()
+
+del check_config
+del REQUIRED
